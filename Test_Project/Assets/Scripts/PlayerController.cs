5912ea4ebb87f09ff8f5605ad8d155e06b801091
neur0genesis <mskm14@student.aau.dk>
2015-03-31 11:04:15 +0200

    Initial project

---
+++ Test_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Start() {
+		// Setup the reference to the alarm light.
+		rb = GetComponent<Rigidbody> ();
+		}
+
+	// Update is called once before physics calculations
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0,moveVertical);
+		rb.AddForce(movement);
+
+	}
+}
