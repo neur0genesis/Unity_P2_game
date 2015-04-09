59fda002ec7bf55375f5f5fa19f4235ed25d449b
neur0genesis <quezdk@outlook.com>
2015-04-09 23:31:28 +0200

    Updated code
    I'm too tired to write anything

--- Assets/Character_Controller.cs
+++ Assets/Character_Controller.cs
@@ -3,107 +3,101 @@ using System.Collections;
 
 public class Character_Controller : MonoBehaviour {
 
-	public Animator animator;
+	private Animator animator;
+
+	public float speed = 0.5f; 
 
 	SpriteRenderer[] legSprite = new SpriteRenderer[4];
 
-	bool greenLight;
+	bool postTransition;
 	bool done = false;
 	bool check;
 
-	// Use this for initialization
+	// USE THIS FOR INITIALIZATION
 	void Start () {
 
+		animator = GetComponentInChildren<Animator>();
+
+		//Initialize leg-sprites
 		legSprite[0] = GameObject.Find("Left_leg_run").GetComponent<SpriteRenderer>();
 		legSprite[1] = GameObject.Find("Right_leg_run").GetComponent<SpriteRenderer>();
 		legSprite[2] = GameObject.Find("Left_leg_idle").GetComponent<SpriteRenderer>();
 		legSprite[3] = GameObject.Find("Right_leg_idle").GetComponent<SpriteRenderer>();
 
+		//Hide leg-sprites that are not needed
 		legSprite [0].enabled = false; 
 		legSprite [1].enabled = false;
 
 		}
 
-
-
-	// Update is called once per frame
+	// UPDATE IS CALLED ONCE PER FRAME
 	void Update () {
+	
+		controller ();
+		animate ();
+
+	}
+
+
+	//CONTROLLER METHOD FOR THE PLAYER-OBJECT  
+	void controller() {
 
 		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical"); 
 
+		//print (vertical*speed);
 
-		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
-			animator.SetBool ("running", true);
-		} else {
-			animator.SetBool ("running", false);
+		Vector3 movement = new Vector3 (horizontal*speed, 0, vertical*speed);
+		
+		transform.Translate (movement); 
+		
+		if (horizontal > 0) {
+			transform.localScale = new Vector3(-1,1,1);
 		}
+		
+		if (horizontal < 0 ) {
+			transform.localScale = new Vector3(1,1,1);
+		}
+	}
 
-
-
-		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) {
-				legSprite[2].enabled = false;
-				legSprite[3].enabled = false;
-		} 
+	//ANIMATION METHOD FOR THE PLAYER-OBJECT 
+	void animate() {
 
 		if (animator.IsInTransition(0) == true) {
-			greenLight = true; 
+			postTransition = true; 
 		}
 
-		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) {
 
-			print ("BOOOOOOOOOOOM");
+			animator.SetBool ("running", true);
 
-
-			//Display run sprites
+			//Display running sprites
 			legSprite [0].enabled = true;
 			legSprite [1].enabled = true;
 			legSprite [2].enabled = false;
 			legSprite [3].enabled = false;
 
-		} else if (greenLight == true) {
-
-			print("Trans: " +animator.IsInTransition(0));
-			print("Greenlight: " + greenLight);
-
-
-
-			if (animator.IsInTransition(0) && greenLight) {
-				//Hvis animatoren er i transition mens greenlight er sand så 
-				// skal vi finde ud af hvornår animatoren ikke længere er 
-				// i transistion og så køre koden. Derefter er greenlight ikke 
-				// sand
-
+		} 	else if (animator.IsInTransition(0) && postTransition) {
+				
 				check = true;
-			}
-
+		} 	else {
+			animator.SetBool ("running", false);
+		}
+			
 			if (check && !animator.IsInTransition(0)) {
-				print("HEEEEEEEEEEEEEEEEEEEEY");
-
+				
 				//Display idle sprites
 				legSprite [0].enabled = false;
 				legSprite [1].enabled = false;
 				legSprite [2].enabled = true;
 				legSprite [3].enabled = true;
-
+				
 				check = false;
 			}
-		} 
-
-		if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.D)) {
-			greenLight = true;
-		}
-
-		Vector2 movement = new Vector3 (horizontal/2, 0, 0);
-
-		transform.Translate (movement); 
 
 
-		if (horizontal > 0) {
-			transform.localScale = new Vector3(-1,1,1);
-					}
 
-		if (horizontal < 0) {
-			transform.localScale = new Vector3(1,1,1);
-		}
 	}
+
+
 }
---
+++ Assets/Shop.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shop : MonoBehaviour {
+
+	private Transform player; 
+	private MeshRenderer renderer; 
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindWithTag ("Player").transform;
+		renderer = GetComponent<MeshRenderer>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 relativePos = player.position - transform.position;
+		float distance = relativePos.magnitude;
+
+		print(distance);
+
+		if (distance < 20) {
+			print("Enter shop");
+			renderer.material.color = Color.red;
+		} else {
+			renderer.material.color = Color.white;
+
+		}
+
+
+
+	}
+}
