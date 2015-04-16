50f62b588fcb50b1ae1ba96bdfa68d9bd20c293d
neur0genesis <mskm14@student.aau.dk>
2015-04-16 15:37:06 +0200

    Changelog
    Basic city layout created

---
+++ Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controller : MonoBehaviour {
+
+	private Animator animator;
+
+	public float speed = 0.5f; 
+	public float scale;
+	private Transform rig; 
+
+	SpriteRenderer[] legSprite = new SpriteRenderer[4];
+
+	bool postTransition;
+	bool check;
+
+	// USE THIS FOR INITIALIZATION
+	void Start () {
+
+		rig = GameObject.Find ("CharacterRig").transform;
+
+		animator = GetComponentInChildren<Animator>();
+
+		//Initialize leg-sprites
+		legSprite[0] = GameObject.Find("Left_leg_run").GetComponent<SpriteRenderer>();
+		legSprite[1] = GameObject.Find("Right_leg_run").GetComponent<SpriteRenderer>();
+		legSprite[2] = GameObject.Find("Left_leg_idle").GetComponent<SpriteRenderer>();
+		legSprite[3] = GameObject.Find("Right_leg_idle").GetComponent<SpriteRenderer>();
+
+		//Hide leg-sprites that are not needed
+		legSprite [0].enabled = false; 
+		legSprite [1].enabled = false;
+
+		}
+
+	// UPDATE IS CALLED ONCE PER FRAME
+	void Update () {
+	
+		controller ();
+		animate ();
+
+	}
+
+
+	//CONTROLLER METHOD FOR THE PLAYER-OBJECT  
+	void controller() {
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical"); 
+
+
+
+		//print (vertical*speed);
+
+		Vector3 movement = new Vector3 (horizontal*speed, 0, vertical*speed);
+		
+		transform.Translate (movement); 
+		
+		if (horizontal > 0) {
+			rig.transform.localScale = new Vector3(-scale,scale,scale);
+		}
+		
+		if (horizontal < 0 ) {
+			rig.transform.localScale = new Vector3(scale,scale,scale);
+		}
+	}
+
+	//ANIMATION METHOD FOR THE PLAYER-OBJECT 
+	void animate() {
+
+		if (animator.IsInTransition(0) == true) {
+			postTransition = true; 
+		}
+
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) {
+
+			animator.SetBool ("running", true);
+
+			//Display running sprites
+			legSprite [0].enabled = true;
+			legSprite [1].enabled = true;
+			legSprite [2].enabled = false;
+			legSprite [3].enabled = false;
+
+		} 	else if (animator.IsInTransition(0) && postTransition) {
+				
+				check = true;
+		} 	else {
+			animator.SetBool ("running", false);
+		}
+			
+			if (check && !animator.IsInTransition(0)) {
+				
+				//Display idle sprites
+				legSprite [0].enabled = false;
+				legSprite [1].enabled = false;
+				legSprite [2].enabled = true;
+				legSprite [3].enabled = true;
+				
+				check = false;
+			}
+
+
+
+	}
+
+
+}
---
+++ Assets/Simple_SeeThrough/Shaders/SimpleSeeThrough_Props_3D.shader
@@ -0,0 +1,62 @@
+﻿Shader "Simple SeeThrough/Props 3D"
+{
+	Properties
+	{
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+
+Category 
+{
+	SubShader
+	{
+	   //Tags {"Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+	    Tags { "RenderType"="Opaque" }
+        Pass
+        {
+
+       // Blend SrcAlpha OneMinusSrcAlpha
+		Cull Off Lighting Off ZWrite On
+		//AlphaTest Greater 0.1
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			fixed4 _MainTex_ST;
+
+			struct appdata_t
+			{
+				fixed4 vertex : POSITION;
+				fixed2 texcoord : TEXCOORD0;
+				//fixed4 color : COLOR;
+			};
+
+			struct v2f
+			{
+				fixed4 vertex : SV_POSITION;
+				fixed2 texcoord : TEXCOORD0;
+				//fixed4 color : COLOR;
+			};
+
+			v2f vert (appdata_t v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				//o.color = v.color;
+				o.texcoord = TRANSFORM_TEX(v.texcoord,_MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : COLOR
+			{
+				fixed4 tex = tex2D(_MainTex, i.texcoord);
+				return tex;
+			}
+			
+			ENDCG
+		}
+	}//FallBack "Diffuse"
+}
+}
